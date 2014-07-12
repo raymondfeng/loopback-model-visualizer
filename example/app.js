74acb86159920855ee8f4b7bc117feaa7d086e36
Raymond Feng <raymond@strongloop.com>
2014-07-12 16:18:55 -0700

    Add loopback UML support

--- example/app.js
+++ example/app.js
@@ -1,76 +1,45 @@
 var fs = require('fs');
 var path = require('path');
 var index = require('../lib/index');
-var viz = index.VizVisualizer;
+var viz = index.ModelVisualizer;
 
-var loopbackData = require('loopback-datasource-juggler');
-var DataSource = loopbackData.DataSource;
-var ds = new DataSource('memory');
-
-// define models
-var Post = ds.define('Post', {
-    title: { type: String, length: 255 },
-    content: { type: DataSource.Text },
-    date: { type: Date, default: function () {
-        return new Date;
-    } },
-    timestamp: { type: Number, default: Date.now },
-    published: { type: Boolean, default: false, index: true }
-});
-
-var UserSchema = {
-    name: String,
-    bio: DataSource.Text,
-    approved: Boolean,
-    joinedAt: Date,
-    age: Number
-};
-
-var User = ds.define('User', UserSchema);
-
-// setup relationships
-User.hasMany(Post, {as: 'posts', foreignKey: 'userId'});
-
-var user = new User({name: 'Joe', age: 30, joinedAt: new Date()});
-
-// console.log(ds.models);
-// console.log(ds.definitions);
+var loopback = require('loopback');
+var ds = loopback.memory();
+loopback.setDefaultDataSourceForType('db', ds);
+loopback.setDefaultDataSourceForType('mail', loopback.createDataSource({connector: 'mail'}));
+loopback.autoAttach();
 
 var options = {
-    builtins: false,
-    allFunctions: false,
-    excludingNulls: true,
-    format: 'svg'
+  excludingNulls: true,
+  format: 'svg'
 };
 
+var models = [];
+var modelReg = loopback.Model.modelBuilder.models;
+for (var m in modelReg) {
+  if(m.indexOf('AnonymousModel_') === 0) {
+    continue;
+  }
+  models.push(modelReg[m]);
+}
 console.log('Rendering diagrams ...');
 
-var schemaSvg = viz.render('UserSchema', UserSchema, options);
+var schemaSvg = viz.render('Models', models, options);
 
-fs.writeFile(path.join(__dirname, 'user-schema.svg'), schemaSvg, function (err) {
-    if (err) throw err;
-    console.log('user-schema.svg is saved to ' + __dirname);
+fs.writeFile(path.join(__dirname, 'loopback-models.svg'), schemaSvg, function (err) {
+  if (err) {
+    throw err;
+  }
+  console.log('loopback-models.svg is saved to ' + __dirname);
 });
 
-var clsSvg = viz.render('User', User, options);
+var clsGraph = viz.renderGraph('Models', models, options);
 
-fs.writeFile(path.join(__dirname, 'user-class.svg'), clsSvg, function (err) {
-    if (err) throw err;
-    console.log('user-class.svg is saved to ' + __dirname);
-});
-
-var objSvg = viz.render('User', user, options);
-
-fs.writeFile(path.join(__dirname, 'user-object.svg'), objSvg, function (err) {
-    if (err) throw err;
-    console.log('user-object.svg is saved to ' + __dirname);
-});
-
-var clsGraph = viz.renderGraph('User', User, options);
-
-fs.writeFile(path.join(__dirname, 'user-class.dot'), clsGraph, function (err) {
-    if (err) throw err;
-    console.log('user-class.dot is saved to ' + __dirname);
+fs.writeFile(path.join(__dirname, 'loopback-models.dot'), clsGraph, function (err) {
+  if (err) {
+    throw err;
+  }
+  console.log('loopback-models.dot is saved to ' + __dirname);
 });
 
 
